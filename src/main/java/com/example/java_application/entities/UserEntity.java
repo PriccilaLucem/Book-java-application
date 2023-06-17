b348c4f0d70cf1b17925468785b4e4d1c9588911
Divinity <systemdivinity@gmail.com>
2023-06-17 17:34:22 -0300

    feat/userEntity

---
+++ src/main/java/com/example/java_application/entities/UserEntity.java
@@ -0,0 +1,58 @@
+package com.example.java_application.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "users")
+public class UserEntity implements Serializable{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+    private String password;
+    private String name;
+
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
---
+++ src/main/java/com/example/java_application/entities/validateUser.java
@@ -0,0 +1,105 @@
+package com.example.java_application.entities;
+
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "validate_user")
+public class validateUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long id;
+    @Column(name = "is_valid")
+    private Boolean isValid = false;
+    @Column(name = "valdated_date")
+    private Date validatedDate;
+
+
+
+    public validateUser() {
+    }
+
+    public validateUser(Long id, Boolean isValid, Date validatedDate) {
+        this.id = id;
+        this.isValid = isValid;
+        this.validatedDate = validatedDate;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean isIsValid() {
+        return this.isValid;
+    }
+
+    public Boolean getIsValid() {
+        return this.isValid;
+    }
+
+    public void setIsValid(Boolean isValid) {
+        this.isValid = isValid;
+    }
+
+    public Date getValidatedDate() {
+        return this.validatedDate;
+    }
+
+    public void setValidatedDate(Date validatedDate) {
+        this.validatedDate = validatedDate;
+    }
+
+    public validateUser id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public validateUser isValid(Boolean isValid) {
+        setIsValid(isValid);
+        return this;
+    }
+
+    public validateUser validatedDate(Date validatedDate) {
+        setValidatedDate(validatedDate);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof validateUser)) {
+            return false;
+        }
+        validateUser validateUser = (validateUser) o;
+        return Objects.equals(id, validateUser.id) && Objects.equals(isValid, validateUser.isValid) && Objects.equals(validatedDate, validateUser.validatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, isValid, validatedDate);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", isValid='" + isIsValid() + "'" +
+            ", validatedDate='" + getValidatedDate() + "'" +
+            "}";
+    }
+    
+}
