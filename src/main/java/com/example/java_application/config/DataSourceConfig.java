4a2daa88de2e0cfca75df14372f71df93a6d0c78
Divinity <systemdivinity@gmail.com>
2023-06-17 16:29:45 -0300

    feat/initingProject

---
+++ src/main/java/com/example/java_application/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package  com.example.java_application.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource datasource() {
+        return DataSourceBuilder.create()
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .url("jdbc:mysql://localhost:3306/myDatabase")
+                .username("root")
+                .password("12345678")
+                .build();
+    }
+}
