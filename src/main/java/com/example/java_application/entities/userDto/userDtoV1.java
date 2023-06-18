3986a1cb8f73ff81cef7633210cdc422ea0e60e3
Divinity <systemdivinity@gmail.com>
2023-06-18 10:49:07 -0300

    feat/userDto

---
+++ src/main/java/com/example/java_application/entities/userDto/userDtoV1.java
@@ -0,0 +1,115 @@
+package com.example.java_application.entities.userDto;
+
+import com.example.java_application.entities.validateUser;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.Objects;
+
+public class userDtoV1 {
+    private Long id;
+    private String nickName;
+    private String email;
+    @JsonIgnore
+    private String password;
+    private validateUser validateUser;
+
+    public userDtoV1() {
+    }
+
+    public userDtoV1(String email, Long id, String password, validateUser validateUser) {
+        this.email = email;
+        this.id = id;
+        this.password = password;
+        this.validateUser = validateUser;
+    }
+
+
+    public String getNickName() {
+        return this.nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public validateUser getValidateUser() {
+        return this.validateUser;
+    }
+
+    public void setValidateUser(validateUser validateUser) {
+        this.validateUser = validateUser;
+    }
+
+    public userDtoV1 email(String email) {
+        setEmail(email);
+        return this;
+    }
+
+    public userDtoV1 id(Long id) {
+        setId(id);
+        return this;
+    }
+
+    public userDtoV1 password(String password) {
+        setPassword(password);
+        return this;
+    }
+
+    public userDtoV1 validateUser(validateUser validateUser) {
+        setValidateUser(validateUser);
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof userDtoV1)) {
+            return false;
+        }
+        userDtoV1 userDtoV1 = (userDtoV1) o;
+        return Objects.equals(email, userDtoV1.email) && Objects.equals(id, userDtoV1.id) && Objects.equals(password, userDtoV1.password) && Objects.equals(validateUser, userDtoV1.validateUser);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, id, password, validateUser);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " email='" + getEmail() + "'" +
+            ", id='" + getId() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", validateUser='" + getValidateUser() + "'" +
+            "}";
+    }
+    
+
+}
